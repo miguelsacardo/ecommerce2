d1ab08e7ce95cd6a3f831476a87a2b3908460eaf
Miguel Sacardo Lalla Rosa <51773121863@>
2024-09-13 16:57:29 -0300

    13/09/24 v1

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    codigoProduto = models.IntegerField(max_length=10)
+    tituloProduto = models.CharField(max_length=255)
+    preco = models.DecimalField(max_length=10, decimal_places=2)
